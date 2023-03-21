8ebd5bb6340d33bb5fe656dbeac472b6d0b88e37
Beatriz <beatrizhelena777@gmail.com>
2023-03-21 02:00:29 -0300

    alteração responsividade

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { ReactiveFormsModule } from '@angular/forms';
 import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatNativeDateModule} from '@angular/material/core';
+import {MatCardModule} from '@angular/material/card';
+
+
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ReminderComponent } from './Forms/reminder/reminder.component';
 import { MenuComponent } from './Components/menu/menu.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 @NgModule({
   declarations: [
@@ -18,7 +28,13 @@ import { MenuComponent } from './Components/menu/menu.component';
     BrowserModule,
     AppRoutingModule,
     ReactiveFormsModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    BrowserAnimationsModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
